4d93112d1d30ba6b003e4cbf70876ad4759e42e7
greteik <greteikhod@gmail.com>
2025-05-13 20:30:47 +0300

    update 6.0

--- js/main.js
+++ js/main.js
@@ -12,3 +12,48 @@ document.addEventListener('keydown', (e) => {
   }
 });
 
+document.querySelectorAll('.menu__link').forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault(); 
+    const targetId = link.getAttribute('href');
+    document.querySelector(targetId).scrollIntoView({
+      behavior: 'smooth' 
+    });
+    
+    menu.classList.remove('menu--open');
+  });
+});
+
+
+const button = document.querySelector('.header__button');
+const modal = document.getElementById('template-modal');
+const closeBtn = modal.querySelector('.modal__close');
+
+button.addEventListener('click', function(e) {
+  e.preventDefault();
+  modal.style.display = 'flex';
+});
+
+closeBtn.addEventListener('click', function() {
+  modal.style.display = 'none';
+});
+
+
+window.addEventListener('click', function(e) {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+});
+
+document.querySelector('.header__button').addEventListener('click', function(e) {
+  e.preventDefault();
+  
+
+
+  const link = document.createElement('a');
+  link.href = '"C:\Users\ddv\Downloads\programmirov.webp"'; 
+  link.download = 'programmirov.webp'; 
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+});
