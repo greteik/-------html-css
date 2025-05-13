8bbde54551a66b10b29232b9e5b967be62602734
greteik <greteikhod@gmail.com>
2025-05-13 19:08:30 +0300

    update 5.0

--- js/main.js
+++ js/main.js
@@ -4,3 +4,11 @@ const menu = document.querySelector('.menu');
 menuBtn.addEventListener('click', ()=> {
     menu.classList.toggle('menu--open')
 });
+
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && menu.classList.contains('menu--open')) {
+    menu.classList.remove('menu--open');
+  }
+});
+
